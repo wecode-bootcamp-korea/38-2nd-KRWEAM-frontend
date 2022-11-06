75a4e557b13443c6996805e4ae549df658b9666b
yoonsue <mrokr0328@gmail.com>
2022-11-07 00:24:42 +0900

    ProductList -Collapse 필터 구현 -무한 스크롤 구현 -관심상품 상태 반영 추가

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const APIKEY = process.env.REACT_APP_API_KEY;
 export const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
 
 const BASE_URL_TS = "http://10.58.52.214:3000";
-const BASE_URL_CW = "http://10.58.52.112:3000";
+const BASE_URL_CW = "http://10.58.52.94:3000";
 
 export const api = {
   wish: `${BASE_URL_TS}/wish`,
